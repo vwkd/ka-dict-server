47d2ffbe72f7e045d8d1b79902bfd4cc6121ee11
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-06 13:35:41 +0400

    database: import entries from JSON

--- src/database.ts
+++ src/database.ts
@@ -1,45 +1,11 @@
-const entriesArray = [
-  { id: 0,
-    source: {
-      value: "ბუ",
-      meaning: 2,
-    },
-    target: [
-      {
-        value: ["aufschütteln"],
-        meaning: 1,
-        tags: ["KACH"],
-      },
-      {
-        value: ["anschwellen"],
-        meaning: 2,
-        tags: ["KACH"],
-      }
-    ],
-  },
-  { id: 1,
-    source: {
-      value: "ახლ",
-      meaning: 3,
-    },
-    reference: {
-      id: 99,
-      source: {
-        value: "ხლ",
-        meaning: 3,
-      },
-      kind: "DIRECT",
-      tags: [],
-    },
-  },
-];
+import entries from "./vz.json" assert { type: "json" };
 
 function entry(id) {
-  return entriesArray.find(entry => entry.id == id);
+  return entries.find(entry => entry.id == id);
 }
 
 function findEntries(term) {
-  return entriesArray.filter(entry => entry.source.value.includes(term) || entry.target?.some(e => e.value.some(w => w.includes(term))) || entry.reference?.source.value.includes(term));
+  return entries.filter(entry => entry.source.value.includes(term) || entry.target?.some(e => e.value.some(w => w.includes(term))) || entry.reference?.source.value.includes(term));
 }
 
 const database = {
