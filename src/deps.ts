13583895b351ed4fc97b06e813e80a5178b1ac54
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-11 21:00:33 +0400

    database: use FuseJS search

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,3 @@
 export { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 export { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLNonNull, GraphQLInt, GraphQLList, GraphQLEnumType, GraphQLUnionType, printSchema } from 'https://cdn.skypack.dev/graphql';
+export { default as Fuse } from "https://cdn.skypack.dev/fuse.js";
