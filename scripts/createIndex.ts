045d497c6ba107c7ab40d8c845ec7f804e39dcb6
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-11 21:09:05 +0400

    scripts: add create index

---
+++ scripts/createIndex.ts
@@ -0,0 +1,12 @@
+import entries from "./vz.json" assert { type: "json" };
+import { Fuse } from "../src/deps.ts";
+
+const keys = [
+  "source.value",
+  "target.value.source.value",
+  "target.value.value",
+];
+
+const index = Fuse.createIndex(keys, entries);
+
+await Deno.writeTextFile("data/index.json", JSON.stringify(index));
