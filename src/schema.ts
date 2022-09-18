0212ad27e4f8046fdb0261699b040771a61d138b
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-18 06:30:24 +0400

    schema: update grammar, meaning on references

--- src/schema.ts
+++ src/schema.ts
@@ -119,6 +119,9 @@ const referenceType = new GraphQLObjectType({
     source: {
       type: new GraphQLNonNull(sourceType),
     },
+    meaning: {
+      type: GraphQLInt,
+    },
     kind: {
       type: new GraphQLNonNull(kindType),
     },
