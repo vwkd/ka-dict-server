c99aea8b255ee747dd8168c5691904ba51f0ebff
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-05 13:23:39 +0400

    database: filter search results

--- src/deps.ts
+++ src/deps.ts
@@ -5,11 +5,14 @@ import Fuse from "https://cdn.skypack.dev/fuse.js";
 export { Fuse };
 import entries from "https://raw.githubusercontent.com/vwkd/ka-dict-parser/output/vz.json" assert { type: "json" };
 export { entries };
+export { deepMerge } from "https://raw.githubusercontent.com/vwkd/code-web-utilities/d29b456b80de1f60b349238908b86bd83ebcb3b0/src/deep_merge.ts";
+export { deepFilter } from "https://raw.githubusercontent.com/vwkd/code-web-utilities/d29b456b80de1f60b349238908b86bd83ebcb3b0/src/deep_filter.ts";
 
 export const fuse_options = {
   threshold: 0,
   ignoreLocation: true,
   minMatchCharLength: 2,
+  includeMatches: true,
   keys: [
     "source.value",
     "target.value.source.value",
