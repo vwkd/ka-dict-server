b44bf01374f932456a864906725ec4ee0b93dbd7
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-05 17:31:29 +0400

    FIXUP database: use FuseJS search

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,7 @@
 export { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 export { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLNonNull, GraphQLInt, GraphQLList, GraphQLEnumType, GraphQLUnionType, printSchema } from 'https://cdn.skypack.dev/graphql';
 
-import Fuse from "https://cdn.skypack.dev/fuse.js";
+import Fuse from "https://cdn.skypack.dev/fuse.js@6.6.2";
 export { Fuse };
 import entries from "https://raw.githubusercontent.com/vwkd/ka-dict-parser/output/vz.json" assert { type: "json" };
 export { entries };
